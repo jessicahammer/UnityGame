dbd12dcb58233b260455ed39cc925cdac6aa6d2b
Jessica Hammer <hammer.jessica4@gmail.com>
2019-06-09 00:29:15 +0200

    level complete screen

---
+++ UnityGameP6/Assets/GM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+    public static float vertVel=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
